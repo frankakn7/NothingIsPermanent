5a8c3d9d522e8e9e81794513a05a827aafe8aefb
Maxim Strzebkowski <maxim_st@gmx.de>
2021-11-30 10:39:04 +0100

    deletes not needed corridors however still problem with double generation when corridor is there from one room another that wasnt generated by that room can generate another corridor ontop of the others

--- Game_Files/RoomManager.gd
+++ Game_Files/RoomManager.gd
@@ -11,6 +11,9 @@ var room_layout = [	[1, 1, 1, 1],
 					[1, 1, 1, 1] ]
 					
 var rooms = []
+var corridors = []
+
+var neededRooms = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,6 +30,8 @@ func generateNeighbourRooms(id, roomPos):
 
 func generateRoom(offsetVector, parentPosition, parentId):
 	var room_id = parentId + offsetVector
+	if !(room_id in neededRooms):
+		neededRooms.append(room_id)
 	if room_id in rooms:
 		return			#Room already exists
 	var room_instance = room_scene.instance()
@@ -43,27 +48,30 @@ func generateRoom(offsetVector, parentPosition, parentId):
 	var offset_to_room_center_X = (room_instance.tile_pixel_width * room_instance.max_width) / 2
 	var offset_to_room_center_Y = (room_instance.tile_pixel_width * room_instance.max_height) / 2
 	room_instance.global_position = Vector2(parentPosition.x + offset_to_room_center_X, parentPosition.y + offset_to_room_center_Y)  + Vector2(offset_to_parent_X - offset_to_room_center_X, offset_to_parent_Y - offset_to_room_center_Y)
-	print("(%d,%d) didnt generate corridor" % [room_id.x, room_id.y], offsetVector)
-	generateCorridors(room_instance.global_position, room_width, room_height, [- offsetVector])
+	#print("(%d,%d) didnt generate corridor" % [room_id.x, room_id.y], offsetVector)
+	generateCorridors(room_id, room_instance.global_position, room_width, room_height, [- offsetVector])
 	room_instance.connect("player_entered", self, "on_Enter")
 	room_instance.get_node("DebugControl/DebugRoomNB").text = "%d/%d" % [room_id.x, room_id.y]
 
-func generateCorridor(roomPos, side, roomWidth, roomHeight):
+func generateCorridor(roomId, roomPos, side, roomWidth, roomHeight):
 	#1/2 of room width + distance 1/2
 	var offsetX = side.x * ((roomWidth / 2) + room_distance / 2)
 	var offsetY = side.y * ((roomHeight / 2) + room_distance / 2)
 	var roomCenter = Vector2(roomPos.x + roomWidth / 2, roomPos.y + roomHeight / 2)
 	var corridor = corridor_hor_scene.instance()
+	var idString = "({fromX},{fromY})-({toX},{toY})".format({"fromX":roomId.x,"fromY":roomId.y, "toX":roomId.x + side.x, "toY":roomId.y + side.y})
+	corridor.set_name(idString)
 	add_child(corridor)
+	corridors.append(idString)
 	corridor.global_position = Vector2(roomCenter.x + offsetX, roomCenter.y + offsetY)
 	corridor.global_rotation = side.angle()
 
-func generateCorridors(roomPos, roomWidth, roomHeight, exceptSides=[]):
+func generateCorridors(roomId, roomPos, roomWidth, roomHeight, exceptSides=[]):
 	var sides = [Vector2(-1,0), Vector2(0,-1), Vector2(1,0), Vector2(0,1)]
 	for side in sides:
 		if side in exceptSides:
 			continue
-		generateCorridor(roomPos, side, roomWidth, roomHeight)
+		generateCorridor(roomId, roomPos, side, roomWidth, roomHeight)
 	
 	
 
@@ -72,6 +80,7 @@ func generateStartRoom(id):
 	room_instance.set_name("Room_%d_%d" % [id.x,id.y])
 	add_child(room_instance)
 	rooms.append(id)
+	neededRooms.append(id)
 	room_instance.id = id
 	var room_width = room_instance.tile_pixel_width * room_instance.max_width
 	var room_height = room_instance.tile_pixel_width * room_instance.max_height
@@ -79,12 +88,44 @@ func generateStartRoom(id):
 	var offsetY = (room_height) / 2
 	room_instance.global_position = Vector2(0 - offsetX, 0 - offsetY)
 	room_instance.connect("player_entered", self, "on_Enter")
-	generateCorridors(room_instance.global_position, room_width, room_height)
+	generateCorridors(id, room_instance.global_position, room_width, room_height)
 	room_instance.get_node("DebugControl/DebugRoomNB").text = "%d/%d" % [id.x, id.y]
 
 func on_Enter(id, roomPos):
 	#print(rooms)
+	neededRooms = [id]
 	generateRoom(Vector2(0,1), roomPos, id)
 	generateRoom(Vector2(1,0), roomPos, id)
 	generateRoom(Vector2(-1,0), roomPos, id)
 	generateRoom(Vector2(0,-1), roomPos, id)
+	deleteNotNeededRooms()
+	deleteNotNeededCorridors()
+
+func deleteNotNeededRooms():
+	for id in neededRooms:
+		rooms.erase(id)
+	
+	for id in rooms:
+		get_node("Room_%d_%d" % [id.x,id.y]).queue_free()
+	
+	rooms = neededRooms
+
+func deleteNotNeededCorridors():
+	var keep = []
+	var regex = RegEx.new()
+	var regexString = ""
+	for id in rooms:
+		var idString = "\\(%d,%d\\)" % [id.x, id.y]
+		if regexString.empty():
+			regexString = idString
+		else:
+			regexString = regexString + "|" + idString
+	
+	regex.compile(regexString)
+	
+	for id in corridors:
+		if not regex.search(id):
+			get_node(id).queue_free()
+		else:
+			keep.append(id)
+	corridors = keep
